2a9ac0d63a23fb4c8507fe2ef0377a4d5bb9a4cc
张航 <z_h0715@163.com>
2020-04-05 22:31:21 +0800

    Change main.js url

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ import echarts from 'echarts'
 Vue.use(ElementUI)
 Vue.config.productionTip = false
 Vue.use(VueAxios, axios)
-axios.defaults.baseURL = 'http://192.168.0.165:8090'
+axios.defaults.baseURL = 'http://101.37.173.73:8090/'
 axios.defaults.headers.post['Content-Type'] = 'application/json;charset=UTF-8'
 Vue.prototype.$echarts = echarts
 
